b1944d8c16a6b7379e64726f6884cd3a7c0bc973
heathercostello <heathercostello@byui.edu>
2019-06-01 19:55:19 -0700

    change

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -1,6 +1,10 @@
-<script type="text/javascript">
-    date();
-    function date() {
-    document.getElementById('mydate').innerHTML = "your date code"
-    };
-</script>
+function getDate() {
+    var d = new Date();
+    var strArray=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var g = date.getDate();
+    var m = strArray[date.getMonth()];
+    var y = date.getFullYear();
+    return '' + (g <= 9 ? '0' + g : g) + '-' + m + '-' + y;
+    document.getElementById('showDate').innerHTML = d;
+};
+getDate();
