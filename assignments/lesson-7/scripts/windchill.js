fc6598202b2de33bcfef1279fc833ffa5e65c242
heathercostello <heathercostello@byui.edu>
2019-06-03 15:23:11 -0700

    yes

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,3 +1,6 @@
-function getWindChill() {
-    
-}
+let t = parseInt(document.getElementById('temp').textContent);
+let s = parseInt(document.getElementById('windspeed').textContent);
+
+let result = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, .16)) + (.4275 * t * Math.pow(s, .16));
+
+document.getElementById('output').innerHTML = "Wind Chill: <strong>" + result.toFixed(1) + "</strong>";
