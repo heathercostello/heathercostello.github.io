b54045c4a6bdbb0f61eed87962d6ee5f4adffe56
heathercostello <heathercostello@byui.edu>
2019-07-26 20:19:25 -0700

    optimize

--- final-project/scripts/weatherstatsrome.js
+++ final-project/scripts/weatherstatsrome.js
@@ -15,3 +15,20 @@ weatherObject.onload = function() {
 
     calculatewc();
 }
+
+// Temple Closure JSON
+var article = document.querySelector('article');
+var requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    var townData = request.response;
+    showData(townData);
+}
+
+function showData(jsonObj) {
+    var data = jsonObj['templeClosureDates'];
+
+}
