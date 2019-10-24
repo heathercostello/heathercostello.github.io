04d27c2b1953792d0d6030453631c94c40143383
costelloheather <heathercostello6@gmail.com>
2019-10-23 20:19:16 -0700

    8

--- CIT261/scripts/cssmanipulation.js
+++ CIT261/scripts/cssmanipulation.js
@@ -12,3 +12,21 @@ function changeFontSize() {
 function adjustPixels(pixels) {
     document.getElementById("textSliderOutput").innerHTML = pixels + ' px';
 }
+
+
+
+function myMove() {
+    var elem = document.getElementById("animate");
+    var pos = 0;
+    var id = setInterval(frame, 5);
+
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    }
+}
